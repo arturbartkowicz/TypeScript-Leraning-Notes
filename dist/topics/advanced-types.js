e25d3d28741bb9fb0ce814274aacf744d803c3e0
Artur Bartkowicz <artur_bartkowicz@epam.com>
2022-06-21 15:25:10 +0200

    Init commit

---
+++ dist/topics/advanced-types.js
@@ -0,0 +1,15 @@
+"use strict";
+let users = [
+    {
+        name: 'John Smith',
+        age: 30,
+        occupation: 'Software Engineer'
+    },
+    {
+        name: 'Kate Muller',
+        age: 28
+    }
+];
+let day = 'Monday';
+console.log(day);
+//# sourceMappingURL=advanced-types.js.map
---
+++ src/topics/advanced-types.ts
@@ -0,0 +1,71 @@
+// Exercises
+
+// 1 Given the data below, define a type alias for representing users
+
+let users = [
+	{
+		name: 'John Smith',
+		age: 30,
+		occupation: 'Software Engineer'
+	},
+	{
+		name: 'Kate Muller',
+		age: 28
+	}
+]
+
+// 1 Solution
+
+type User = {
+	name: string,
+	age: number,
+	occupation?: string
+}
+
+// 2 Birds fly. Fish swim. A Pet can be a Bird or Fish. Use type aliases to represent these
+
+// 2 Soultion
+
+type Bird = {
+	fly: () => void
+}
+
+type Fish = {
+	swim: () => void
+}
+
+type Pet = Bird | Fish
+
+// 3 Define a type for representing the days of week. Valid values are “Monday”, “Tuesday”, etc.
+
+// 3 Solution
+
+type DayOfWeek = 'Monday' | 'Tuesday' | 'Wednesday'
+
+let day: DayOfWeek = 'Monday'
+
+console.log(day)
+
+// 4 Simplify the following code snippets
+
+let user = getUser()
+console.log(user && user.address ? user.address.street : undefined);
+
+let x = foo !== null && foo !== undefined ? foo : bar()
+
+// 4 Solution
+
+console.log( user?.adress?.street);
+let x = foo ?? bar()
+
+// 5 What is the problem in this piece of code?
+
+let value: unknown = 'a';
+console.log(value.toUpperCase())
+
+// 5 Solution
+
+// value is unknow, so we have to do a narrowing for specify the type.
+let value: unknown = 'a';
+if (typeofvalue === 'string')
+console.log(value.toUpperCase());
