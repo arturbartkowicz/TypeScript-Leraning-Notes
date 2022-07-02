0364fde3fa19f6ace5a80abcd07d9af2bc47335e
Artur Bartkowicz <artur.bartkowicz1@gmail.com>
2022-07-02 08:13:47 +0200

    Add generic file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-var age = 20;
-if (age < 50)
-    age += 10;
-console.log(age);
-console.log('ahsbd');
+var colors = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
+var numbers = [0, 1];
+function decodedValue(colorsCollection) {
+    var collection = [];
+    numbers.forEach(function (number) {
+        collection.push(colors.indexOf(colorsCollection[number]));
+    });
+    return parseInt(collection.join(""));
+}
