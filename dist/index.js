59827bac23475a2bb130f5562abc4859af2fa785
Artur Bartkowicz <artur_bartkowicz@epam.com>
2022-07-04 16:05:09 +0200

    Mosh code

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,7 @@
 "use strict";
+let COLORS = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white'];
+function decodedValue([color1, color2]) {
+    return COLORS.indexOf(color1) * 10 + COLORS.indexOf(color2);
+}
+decodedValue(['red', 'red']);
 //# sourceMappingURL=index.js.map
