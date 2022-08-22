362a1f0f1c1016d630af5d748ae5960bf5ff82ca
Artur Bartkowicz <artur_bartkowicz@epam.com>
2022-08-22 13:32:18 +0200

    Add more description to the primitive values

--- dist/topics/01_fundamentals.js
+++ dist/topics/01_fundamentals.js
@@ -1,19 +1,31 @@
 "use strict";
+function log_description(description) {
+    let dashes = ' ---------- ';
+    console.log('');
+    console.log(dashes + description + dashes);
+}
 let numbers1 = [1, 2, 3];
 let user = [10, 'Artur'];
+const small = 1;
+const medium = 2;
+const large = 3;
 var Size;
 (function (Size) {
     Size[Size["Small"] = 0] = "Small";
     Size[Size["Medium"] = 1] = "Medium";
     Size[Size["Large"] = 2] = "Large";
 })(Size || (Size = {}));
+log_description('Log Enum Size.Large');
 console.log(Size.Large);
+let mySize = Size.Medium;
 function calculateTax(income) {
     return 0;
 }
 function calculateTaxTest(income) { }
 let calculatedTax = calculateTax(10);
+log_description('Return value from a function based on the condition');
 console.log(calculatedTax);
 let employeeObject = { id: 10, name: 'Artur' };
+log_description('Log Employee object');
 console.log(employeeObject);
 //# sourceMappingURL=01_fundamentals.js.map
--- src/topics/01_fundamentals.ts
+++ src/topics/01_fundamentals.ts
@@ -8,6 +8,13 @@
 // - Objects
 
 
+function log_description(description: string) {
+  let dashes = ' ---------- '
+  console.log('')
+  console.log(dashes + description + dashes)
+}
+
+
 // ----------     Build-in types     ----------
 
 
@@ -70,17 +77,64 @@
 // ----------     Array     ----------
 
 
+// JS arrays are dynamic, so each element can have a different type.
+// In TS we can explicitly apply a type annotation and ex say a numbers is a number array
+
 let numbers1: number[] = [1, 2, 3]
+
+// W powyższym przykładzie nie musimy wyraźnie określać z czego się składa array, 
+// bo TS to wykryje.Ale np: 
+
+// let numbers = [] - w tym przypadku moglibyśmy mieć mixa różnych typów.
+
+// If you wanna use an empty array you have to explicitly apply a type annotation
+
+// Code Completion or IntelliSense - if you type ex:
+// numbers.forEach(n => n. ('Tutaj za kropką rozwinie się lista dostępnych metod dla array'))
+
+// Because our code editor knows the type of n it offers code completion. So we can 
+// see all the methods of number objects. We don't have that in plain JS.
 
 
 // ----------     Touple      ----------
 
 
+// TS has a new type called touple, which is a fixed-length array, where each 
+// element has a particular type. We often use it working with a pair of values.
+// Ex. For each user you want to represent two values. An id and a name.
+
 let user: [number, string] = [10, 'Artur']
 
+// So we have fixed array with exactly two elements. If we want to add, a third 
+// element that will cause an error. And first one needs to be a number, second a string.
+
+// user[0]. ('It has a code completion as well')
+
+// Trouble method is user.push(1). Compiler will not complain when we want to do that. 
+// This is one of the TS gaps, which may be solved later.
+
+// !!! BEST PRACTISE: restrict a touples to only 2 values, because anything more 
+// than that will become your code hard to understand. ex: !!!
+
+// let user: [number, string, boolean, number] = [2, 'Mosh', true, 40] - messy. 
+// It is hard to tell what these values represent.
+
+// !!! Touples are useful when you have two values. Like key-value pairs !!!
 
 // ----------     Enums     ----------
 
+
+// Enum - represents a list of related constants
+
+// Let's create a group of constants that represents a T-shirt sizes:
+
+const small = 1;
+const medium = 2;
+const large =  3;
+
+// do the same thing using enum:
+
+// Pascal naming convention - first letter on every word should be uppercase.
 
 enum Size {
     Small,
@@ -88,11 +142,36 @@ enum Size {
     Large
 }
 
+log_description('Log Enum Size.Large')
 console.log(Size.Large)
+
+// By default, TS compiler will assign to the first member the value of 0, and for 
+// the rest 1, 2, and so on...
+
+// enum Size { Small = 1, Medium, Large} - we can explicitly assign the first value, 
+// and another one will be 2, and 3...
+
+// enum Size { Small = 's', Medium ='m', Large ='l'} - we can use string values, 
+// but then we need to explicitly assign a value to each member of enum
+
+// Declaratione of a new variable:
+
+let mySize: Size = Size.Medium 
+
+// !!! If we define an enum and use the const keyword before it, the compiler 
+// will generate more optimized JS code. ex.
+
+// const enum Size { Small, Medium, Large}
 
 
 // ----------     Function      ----------
 
+
+// TS compiler has inferred the type of the return value for us (if we will hover 
+// over the function name) 
+// !!! As a best practise we should try to always annotate our functions. 
+// So all the parameters as well as te return type should be properly annotaded 
+// especially when you build an API for other people to use !!!
 
 function calculateTax (income: number): number {
 	return 0;
@@ -104,13 +183,83 @@ function calculateTax (income: number): number {
 
 function calculateTaxTest (income: number): void {}
 
+// When you do that, then when you will not return any value, or the value will be 
+// in a different type, you will get an error immediately.
+
+// In tsconfig:
+// "noUnusedParameters": true - then when the parameter from a function is not 
+// being used, we will get a warning.
+// "noImplicitReturns": true - useful in cases, ex. when in if statement you not 
+// return a specific value under some conditions.
+// "noUnusedLocals": true - warning if you have a local variable, which is not being used  
+
+
+// function calculateTax(income: number, taxYear: number): number {
+//  if (taxYear < 2022)
+//   return income * 1.2;
+//  return income * 1.3
+// }
+
+// calculateTax(10_000, 2022)   - tutaj możemy przekazać tylko 2 parametry, 
+// w przeciwnym wypadku compiler podkreśli błąd
+
+// function calculateTax(income: number, taxYear?: number): number {
+//   if ((taxYear || 2022) < 2022)
+//     return income * 1.2;
+//   return income * 1.3
+// }
+
+// calculateTax(10_000) 
+
+// !!! By adding a question mark '?', we are making a parameter optional !!!
+
+// To make the default value:
+// function calculateTax(income: number, taxYear = 2022): number {
+//   if (taxYear < 2022)
+//     return income * 1.2;
+//   return income * 1.3
+// }
+
+// calculateTax(10_000) 
+
 let calculatedTax = calculateTax(10)
+log_description('Return value from a function based on the condition')
 console.log(calculatedTax)
 
 
 // ----------     Objects       ----------
 
 
+// In JS objects are dynamic, so their shape can change through their lifetime or 
+// programs. So even when we have employee object: 
+
+// let employee = { id: 1 }
+
+// and later we want to add the property name, it is still valid for JS, but not for TS.
+
+// employee.name = 'Mosh'
+
+// We can explicitly type annotation to the object.
+
+// let employee: { 
+//   id: number,
+//   name: string,
+//   fax?: number   -> ? that means, that this is optional
+// }
+
+// Sometimes you want to make certain properties read-only, so we don't 
+// accidentally change them later on.
+
+// let employee: { 
+//   readonly id: number,
+//   name: string,
+//   fax?: number
+// } = { id: 1, name: 'Mosh'}
+  
+// employee.id = 0   - when we add read-only, TS will prevent us from accidentally 
+// change the id in this case
+
 let employeeObject: {  id: number, name: string, fax?: number } = {id: 10, name: 'Artur'}
 
+log_description('Log Employee object')
 console.log(employeeObject)
